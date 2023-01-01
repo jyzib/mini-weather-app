c35af230bc7aced8bb36744894ea65a4a3f8367a
jyzib <121748774+jyzib@users.noreply.github.com>
2023-01-01 15:03:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let weather = prompt('what is weather')
+if (weather == 'rain') {
+    console.log('please bring your umberalla')
+
+}
+else {
+    // console.log('bring your sunglass')
+    alert('bring your sunglass')
+}
